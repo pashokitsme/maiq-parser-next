93234db185f23a7f5172503510b68b0371d734e8
pashokitsme <pashamail79@gmail.com>
2023-09-21 20:43:12 +0300

    chore: display snapshot

--- maiq-cli/src/main.rs
+++ maiq-cli/src/main.rs
@@ -1,4 +1,8 @@
+use maiq_parser_next::snapshot::*;
+
 use tokio::sync::mpsc;
+
+use owo_colors::OwoColorize;
 
 #[tokio::main]
 async fn main() {
@@ -12,8 +16,37 @@ async fn main() {
     .unwrap();
 
   _ = parser.start();
+  let snapshot = rx.recv().await;
+  print_snapshot(&snapshot.unwrap());
+}
 
-  while let Some(value) = rx.recv().await {
-    println!("{value:?}");
+fn print_snapshot(s: &Snapshot) {
+  println!("{} от {}\n", s.id(), s.date());
+  for group in s.groups() {
+    print_group(group);
+    println!()
+  }
+}
+
+fn print_group(g: &Group) {
+  println!("Группа {} ({}) ({})", g.name().bright_white(), g.id().purple(), g.lectures().len());
+  for lecture in g.lectures() {
+    print!("\t");
+    if let Some(ref num) = lecture.order() {
+      print!("{} ", format!("#{}", num).bright_white());
+    }
+    if let Some(sub) = lecture.subgroup() {
+      print!("{} ", format!("(п. {sub})").green())
+    }
+    print!("{} ", lecture.name());
+
+    if let Some(classroom) = lecture.classroom().as_ref() {
+      print!("{} {}", "в".bright_white(), classroom.green());
+    }
+
+    if let Some(teacher) = lecture.teacher().as_ref() {
+      print!(". Преподаватель: {}", teacher.green())
+    }
+    println!()
   }
 }
