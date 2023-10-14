11b7e62cb6d6dce666366bb39f153af38dedf2a4
pashokitsme <pashamail79@gmail.com>
2023-10-14 20:22:17 +0300

    fix(maiq-db): create database if not exists

--- maiq-db/src/error.rs
+++ maiq-db/src/error.rs
@@ -1,10 +1,17 @@
 #[derive(Debug)]
 pub enum Error {
   Sqlx(sqlx::Error),
+  Migrate(sqlx::migrate::MigrateError),
 }
 
 impl From<sqlx::Error> for Error {
   fn from(value: sqlx::Error) -> Self {
     Self::Sqlx(value)
+  }
+}
+
+impl From<sqlx::migrate::MigrateError> for Error {
+  fn from(value: sqlx::migrate::MigrateError) -> Self {
+    Self::Migrate(value)
   }
 }
--- maiq-db/src/lib.rs
+++ maiq-db/src/lib.rs
@@ -5,16 +5,33 @@ mod utils;
 
 use error::Error;
 use log::*;
+use sqlx::migrate::MigrateDatabase;
+use sqlx::Connection;
 
 pub type Pool = sqlx::SqlitePool;
 pub type Result<T> = std::result::Result<T, Error>;
 
 type PoolOptions = sqlx::sqlite::SqlitePoolOptions;
 type Db = sqlx::Sqlite;
+type DbConnection = sqlx::sqlite::SqliteConnection;
 
 pub async fn pool() -> Result<Pool> {
   info!(target: "db", "init sqlite database");
   let url = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+  validate(&url).await?;
   let pool = PoolOptions::new().max_connections(4).connect(&url).await?;
   Ok(pool)
+}
+
+async fn validate(url: &str) -> Result<()> {
+  if !Db::database_exists(url).await? {
+    warn!(target: "db", "database not exists at {}; creating", url);
+    Db::create_database(url).await?;
+  }
+
+  debug!(target: "db", "validating migrations");
+  let mut conn = DbConnection::connect(url).await?;
+  sqlx::migrate!().run(&mut conn).await?;
+
+  Ok(())
 }
