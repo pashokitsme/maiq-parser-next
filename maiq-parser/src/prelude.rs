c3780cea80963a0add19effa95008574387f90fe
pashokitsme <pashamail79@gmail.com>
2023-12-12 01:22:26 +0300

    refactor: remove comments

--- maiq-parser/src/prelude.rs
+++ maiq-parser/src/prelude.rs
@@ -1,17 +1,3 @@
-// pub use crate::parser::r#impl::SnapshotParser4;
-// pub use crate::parser::SnapshotParserBuilder;
-
-// pub use crate::parser::LoopedParserPair;
-// pub use crate::parser::ParserPair;
-
-// pub use crate::parser::SnapshotParserAgent;
-
-// pub use crate::parser::GROUP_NAMES;
-
-// pub use crate::parser::parser_builder;
-// pub use crate::snapshot::*;
-// pub use crate::utils::*;
-
 pub use crate::parser::impls::*;
 pub use crate::parser::repeating::*;
 pub use crate::parser::SnapshotParserAgent;
