4acf8b3c71f8ebc50c63334d81efa33eb4bf37a7
pashokitsme <pashamail79@gmail.com>
2023-12-13 23:30:59 +0300

    fix

--- maiq-bot/src/commands.rs
+++ maiq-bot/src/commands.rs
@@ -100,6 +100,7 @@ impl Commands for Handler {
     let changelogs = changelog::changelog_names()
       .into_iter()
       .map(|(index, name)| [Callback::ChangelogPage { page: index }.with_text(name).into()]);
+    self.delete_message(self.message.chat.id, self.message.id).await?;
     self.reply("Ченджлоги").reply_markup(markup!(changelogs)).await?;
     Ok(())
   }
